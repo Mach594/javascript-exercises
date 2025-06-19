9ed508d71e8fcd45b61ea7bb65fe82c920ac69e4
mach594 <mach594@gmail.com>
2025-06-19 09:38:15 -0400

    Complete exercise 07 in TOP java-exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    repeatedString = ""
+    if (num < 0) {
+        return "ERROR"
+    }
+    while (num > 0) {
+        num--;
+        repeatedString += string;
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
